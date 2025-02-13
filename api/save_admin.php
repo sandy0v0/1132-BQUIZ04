0e1209a40b0d3d5761cfefdbbda30026097dbd5b
sandy0v0 <be0324ss@gmail.com>
2025-02-13 11:26:33 +0800

    解決管理權限忘了勾選時的錯誤

--- api/save_admin.php
+++ api/save_admin.php
@@ -1,7 +1,11 @@
 <?php include_once "db.php";
 // $pr 陣列不能存在資料庫，字串才可以，所以要把pr改寫成字串形式
 
-$_POST['pr']=serialize($_POST['pr']);
+if(!empty($_POST['pr'])){
+    $_POST['pr']=serialize($_POST['pr']);
+}else{
+    $_POST['pr']=serialize([]);
+}
 $Admin->save($_POST);
 
 to("../back.php?do=admin");
