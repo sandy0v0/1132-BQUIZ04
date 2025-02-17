4d4513ad842f875a17800312da9184208d0bbabd
sandy0v0 <be0324ss@gmail.com>
2025-02-17 11:32:39 +0800

    建置詳細資料頁及我要購買按鈕 + 加上我要購買按鈕及點選後帶值到購物車

--- front/main.php
+++ front/main.php
@@ -52,14 +52,19 @@ if($typeId==0){
 foreach($rows as $row):
 ?>
 <div class='item'>
-    <div class='pp'>
+    <div class='pp ct'>
         <a href="?do=detail&id=<?=$row['id'];?>">
             <img src="./img/<?=$row['img'];?>" style="width:200px;height:160px">
         </a>
     </div>
     <div>
         <div class='tt ct'><?=$row['name'];?></div>
-        <div class='pp'>價錢:<?=$row['price'];?></div>
+        <div class='pp'>
+            價錢:<?=$row['price'];?>
+            <a href="?do=buycart&id=<?=$row['id'];?>&qt=1">
+                <img src="./icon/0402.jpg" style="float:right">
+            </a>
+        </div>
         <div class='pp'>規格:<?=$row['spec'];?></div>
         <div class='pp'>簡介:<?=mb_substr($row['intro'],0,20);?>...</div>
     </div>
