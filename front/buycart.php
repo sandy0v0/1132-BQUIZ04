9f92d8c253e162e6c658e37104688e3a36f248f0
sandy0v0 <be0324ss@gmail.com>
2025-02-17 13:48:57 +0800

    建立購物車session及紀錄購物品項機制

--- front/buycart.php
+++ front/buycart.php
@@ -1,7 +1,23 @@
 <?php
+if(!isset($_SESSION['cart'])){
+    $_SESSION['cart']=[];
+}
 
-echo $_GET['id'];
-echo "<br>";
-echo $_GET['qt'];
+if(!empty($_GET['id'])){
+    $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+}
+// dd($_SESSION['cart']);
+
+if(!isset($_SESSION['Mem'])){
+    to("index.php?do=login");
+}
+
+// echo $_GET['id'];
+// echo "<br>";
+// echo $_GET['qt'];
 
 ?>
+
+<h2 class="ct">
+    <?=$_SESSION['Mem'];?>的購物車
+</h2>
