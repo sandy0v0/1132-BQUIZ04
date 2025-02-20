a2ca9e737858d7ab662f3e2d88516b0419dd1082
sandy0v0 <be0324ss@gmail.com>
2025-02-20 10:31:16 +0800

    完成訂單管理的訂單列表及刪除訂單功能

---
+++ back/orders.php
@@ -0,0 +1,30 @@
+<h2 class="ct">訂單管理</h2>
+<!-- table.all>(tr.tt.ct>td*6)+(tr.pp.ct>td*6) -->
+
+<table class="all">
+    <tr class="tt ct">
+        <td>訂單編號</td>
+        <td>金額</td>
+        <td>會員帳號</td>
+        <td>姓名</td>
+        <td>下單時間</td>
+        <td>操作</td>
+    </tr>
+    <?php
+    $rows=$Order->all();
+    foreach($rows as $row):
+    ?>
+    <tr class="pp ct">
+        <td><?=$row['no'];?></td>
+        <td><?=$row['total'];?></td>
+        <td><?=$row['acc'];?></td>
+        <td><?=$row['name'];?></td>
+        <td><?=date("Y/m/d",strtotime($row['order_time']));?></td>
+        <td>
+            <button onclick="del('order',<?=$rows['id'];?>)">刪除</button>    
+        </td>
+    </tr>
+    <?php
+    endforeach;
+    ?>
+</table>
